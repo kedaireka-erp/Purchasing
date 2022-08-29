8d0b2199936be23d2941f3813aa538d1bb1a893a
Yogiana <yogianamartadewi@students.unnes.ac.id>
2022-08-29 22:00:17 +0700

    Layout CSS

--- Purchasing_System/resources/views/layout/sidebar.blade.php
+++ Purchasing_System/resources/views/layout/sidebar.blade.php
@@ -2,19 +2,31 @@
 <html lang="en">
 
 <head>
-    <title>Purchasing System</title>
+
+    {{-- <<========= Ini Title ========>>  --}}
+    <title> @yield('judul-laman') </title>
+    {{-- <<========= Finish Title ========>>  --}}
+
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
+    {{-- <<========= Google Font ========>>  --}}
     <link href="https://fonts.googleapis.com/css?family=Poppins:300,400,500,600,700,800,900" rel="stylesheet">
+    
+    {{-- <<========= Finish Google Font ========>>  --}}
 
+    
+    {{-- <<========= Logo expand ========>>  --}}
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+    {{-- <<========= Finish Logo expand ========>>  --}}
+
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
     <link rel="stylesheet" href="{{asset('layout/layout.css')}}">
 </head>
 
 <body>
-
+ {{-- <<========= Sidebar ========>>  --}}
     <div class="wrapper d-flex align-items-stretch">
         <nav id="sidebar">
 
@@ -31,15 +43,21 @@
              
                 <ul class="list-unstyled components mb-5">
                     <li>
-                        <a href="#">Dasboard</a>
+                        <a href="#">Dashboard</a>
                     </li>
                     <li>
                         <a href="#">Purchase Request</a>
                     </li>
 
                     <li>
-                        <a href="#homeSubmenu" data-toggle="collapse" aria-expanded="false" class="dropdown-toggle">Master Request</a>
-                        <ul class="collapse list-unstyled" id="homeSubmenu">
+                        <a data-bs-toggle="collapse" href="#collapseExample" role="button" aria-expanded="false" aria-controls="collapseExample">
+                            Master Request <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-chevron-down" viewBox="0 0 16 16">
+                                <path fill-rule="evenodd" d="M1.646 4.646a.5.5 0 0 1 .708 0L8 10.293l5.646-5.647a.5.5 0 0 1 .708.708l-6 6a.5.5 0 0 1-.708 0l-6-6a.5.5 0 0 1 0-.708z"/>
+                              </svg>
+                        </a>
+                    </li>
+                      <div class="collapse" id="collapseExample">
+                        <div class="list-request" style="margin-left: 20px">
                             
                             <li>
                                 <a href="#">Master Item</a>
@@ -53,24 +71,32 @@
                             <li>
                                 <a href="#">Master Kebutuhan/Pengiriman</a>
                             </li>
-                        </ul>
-                    </li>
-
+                        </div>
+                      </div>
                     <li>
                         <a href="#">Approval</a>
                     </li>
-   
-                    <li>
-                        <a href="#pageSubmenu" data-toggle="collapse" aria-expanded="false" class="dropdown-toggle">Master Order</a>
-                        <ul class="collapse list-unstyled" id="pageSubmenu">
+
+
+                     <li>
+                        <a data-bs-toggle="collapse" href="#collapseExample2" role="button" aria-expanded="false" aria-controls="collapseExample">
+                            Master Order <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-chevron-down" viewBox="0 0 16 16">
+                                <path fill-rule="evenodd" d="M1.646 4.646a.5.5 0 0 1 .708 0L8 10.293l5.646-5.647a.5.5 0 0 1 .708.708l-6 6a.5.5 0 0 1-.708 0l-6-6a.5.5 0 0 1 0-.708z"/>
+                              </svg>
+                        </a>
+                    </li>
+                      <div class="collapse" id="collapseExample2">
+                        <div class="list-order" style="margin-left: 20px">
+                            
                             <li>
-                                <a href="#">Master Waktu Pengiriman</a>
+                                <a href="#">Master Waktu Peengiriman</a>
                             </li>
                             <li>
                                 <a href="#">Master Pembayaran</a>
                             </li>
                            
-                        </ul>
+                        </div>
+                      </div>
                     </li>
                     <li>
                         <a href="#">Request Tracking</a>
@@ -79,30 +105,12 @@
                         <a href="#">Purchase Order</a>
                     </li>
                 </ul>
-
-                {{-- <div class="mb-5">
-                    <h3 class="h6">Subscribe for newsletter</h3>
-                    <form action="#" class="colorlib-subscribe-form">
-                        <div class="form-group d-flex">
-                            <div class="icon"><span class="icon-paper-plane"></span></div>
-                            <input type="text" class="form-control" placeholder="Enter Email Address">
-                        </div>
-                    </form>
-                </div>
-
-                <div class="footer">
-                    <p>
-                        <!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
-                        Copyright &copy;<script>
-                            document.write(new Date().getFullYear());
-                        </script> All rights reserved | This template is made with <i class="icon-heart" aria-hidden="true"></i> by <a href="https://colorlib.com" target="_blank">Colorlib.com</a>
-                        <!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
-                    </p>
-                </div>
-
-            </div> --}}
         </nav>
 
+         {{-- <<========= Finish Sidebar ========>>  --}}
+
+
+              {{-- <<========= Content ========>>  --}}
         <!-- Page Content  -->
         <div id="content">
            
@@ -113,22 +121,21 @@
                 <div class="container-fluid">
                   <a class="navbar-brand"></a>
                   <form class="d-flex">
-                    {{-- <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
-                    <button class="btn btn-outline-success" type="submit">Search</button> --}}
                   </form>
                 </div>
               </nav>
             <div class="container">
 
 
-            <h2 class="mb-4">Sidebar #02</h2>
-            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+            @yield('Judul-content')
+                
+            <hr>
         </div>
     </div>
 </div>
     <script type="text/javascript" src="{{URL::asset('layout/jquery.min.js')}}"></script>
     <script type="text/javascript" src="{{URL::asset('layout/main.js')}}"></script>
 </body>
+              {{-- <<========= Finish Content ========>>  --}}
 
 </html>
