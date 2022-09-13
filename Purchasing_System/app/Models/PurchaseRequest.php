3cb068af834cc9fa20efffb6bf9d9084484c942d
Rhey_Hsn <rheynaldhasan96@gmail.com>
2022-09-13 18:04:03 +0700

    commit fix edit 2

--- Purchasing_System/app/Models/PurchaseRequest.php
+++ Purchasing_System/app/Models/PurchaseRequest.php
@@ -63,7 +63,7 @@ class PurchaseRequest extends Model
         });
         static::updating(function ($purchase_requests) {
             // $purchase_requests->tanggal_diterima = PurchaseRequest::where('approval_status', 'pending')->update(array('approval_status' => 'approval')) ->update(array('tanggal_diterima' => format("Y-m-d")));
-            
+            // sds
             $purchase_requests->number = PurchaseRequest::where('prefixes_id', $purchase_requests->prefixes_id)->max('number')+1;
             $purchase_requests->no_pr = 'PR'. '/'. $purchase_requests->Prefixe->prefix  . '/' .str_pad($purchase_requests->number, 5, '0', STR_PAD_LEFT) . '/'. Carbon::now()->month . '/'. Carbon::now()->year;
         });
