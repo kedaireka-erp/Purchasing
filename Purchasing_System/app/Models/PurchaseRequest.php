418ae9b0da97f93f65a593091c7d7afe3fee52ea
Alifio4 <fionugroho4@students.unnes.ac.id>
2022-09-19 11:32:03 +0700

    Fixed Field Nomor PR

--- Purchasing_System/app/Models/PurchaseRequest.php
+++ Purchasing_System/app/Models/PurchaseRequest.php
@@ -67,13 +67,127 @@ class PurchaseRequest extends Model
 
         static::creating(function($purchase_requests){
             $purchase_requests->number = PurchaseRequest::where('prefixes_id', $purchase_requests->prefixes_id)->max('number')+1;
-            $purchase_requests->no_pr = 'PR'. '/'. $purchase_requests->Prefixe->prefix  . '/' .str_pad($purchase_requests->number, 5, '0', STR_PAD_LEFT) . '/'. Carbon::now()->month . '/'. Carbon::now()->year;
+            $month = Carbon::now()->month;
+
+            switch ($month) {
+            case "01":
+                $month = "I";
+
+                break;
+            case "02":
+                $month = "II";
+                break;
+            case "03":
+                $month = "III";
+
+                break;
+            case "04":
+                    $month = "IV";
+                
+                    break;
+
+            case "05":
+                $month = "V";
+
+                break;
+
+            case "06":
+                    $month = "VI";
+                
+                    break;
+
+            case "07":
+                    $month = "VII";
+
+                    break;
+            case "08":
+                    $month = "VIII";
+                
+                    break;
+            case "09":
+                $month = "IX";
+
+                break;
+                case "10":
+                    $month = "X";
+                
+                    break;
+                case "11":
+                    $month = "XI";
+                
+                    break;
+                case "12":
+                    $month = "XII";
+                
+                    break;
+            default:
+                echo "Terjadi kesalahan penulisan tanggal";
+            }
+
+            $purchase_requests->no_pr = str_pad($purchase_requests->number, 3, '0', STR_PAD_LEFT) . '/' . 'PR'.'/'.'AA'.'/'.'WS'.$purchase_requests->Prefixe->prefix  . '/'. $month . '/'. Carbon::now()->year;
         });
         static::updating(function ($purchase_requests) {
             // $purchase_requests->tanggal_diterima = PurchaseRequest::where('approval_status', 'pending')->update(array('approval_status' => 'approval')) ->update(array('tanggal_diterima' => format("Y-m-d")));
             
             $purchase_requests->number = PurchaseRequest::where('prefixes_id', $purchase_requests->prefixes_id)->max('number')+1;
-            $purchase_requests->no_pr = 'PR'. '/'. $purchase_requests->Prefixe->prefix  . '/' .str_pad($purchase_requests->number, 5, '0', STR_PAD_LEFT) . '/'. Carbon::now()->month . '/'. Carbon::now()->year;
+            $month = Carbon::now()->month;
+
+            switch ($month) {
+                case "01":
+                    $month = "I";
+    
+                    break;
+                case "02":
+                    $month = "II";
+                    break;
+                case "03":
+                    $month = "III";
+    
+                    break;
+                case "04":
+                        $month = "IV";
+                    
+                        break;
+    
+                case "05":
+                    $month = "V";
+    
+                    break;
+    
+                case "06":
+                        $month = "VI";
+                    
+                        break;
+    
+                case "07":
+                        $month = "VII";
+    
+                        break;
+                case "08":
+                        $month = "VIII";
+                    
+                        break;
+                case "09":
+                    $month = "IX";
+    
+                    break;
+                    case "10":
+                        $month = "X";
+                    
+                        break;
+                    case "11":
+                        $month = "XI";
+                    
+                        break;
+                    case "12":
+                        $month = "XII";
+                    
+                        break;
+                default:
+                    echo "Terjadi kesalahan penulisan tanggal";
+                }
+
+            $purchase_requests->no_pr = str_pad($purchase_requests->number, 3, '0', STR_PAD_LEFT).'/'.'PR'.'/'.'AA'.'/'.'WS'.$purchase_requests->Prefixe->prefix  . '/'. $month . '/'. Carbon::now()->year;
         });
     }
 
