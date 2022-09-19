082751bc2f27e6d76fe91be38dfafc0f02cd69c7
Alifio4 <fionugroho4@students.unnes.ac.id>
2022-09-19 13:08:54 +0700

    Tambah field PO

--- Purchasing_System/database/migrations/2022_09_12_092335_create_item_requests_table.php
+++ Purchasing_System/database/migrations/2022_09_12_092335_create_item_requests_table.php
@@ -19,9 +19,24 @@ return new class extends Migration
             $table->unsignedBigInteger("id_item")->nullable();
             $table->unsignedBigInteger("powder_id")->nullable();
             $table->timestamps();
+            
+            $table->string("no_po")->nullable();
+            $table->string("supplier")->nullable();
+            $table->string("nama_supplier")->nullable();
+            $table->unsignedBigInteger("id_waktu")->nullable();
+            $table->unsignedBigInteger("id_pembayaran")->nullable();
+            $table->string("alamat_penagihan")->nullable();
+            $table->string("lain-lain")->nullable();
+            $table->string("note")->nullable();
+            $table->string("signature")->nullable();
+            $table->string("nama")->nullable();
+
+
             $table->foreign("powder_id")->references("id")->on("powders")->onDelete("restrict")->onUpdate("cascade")->nullable();
             $table->foreign("id_request")->references("id")->on("purchase_requests")->onDelete("restrict")->onUpdate("cascade")->nullable();
             $table->foreign("id_item")->references("id")->on("items")->onDelete("restrict")->onUpdate("cascade")->nullable();
+            $table->foreign("id_waktu")->references("id")->on("timeshippings")->onDelete("restrict")->onUpdate("cascade")->nullable();
+            $table->foreign("id_pembayaran")->references("id")->on("payments")->onDelete("restrict")->onUpdate("cascade")->nullable();
         });
     }
 
