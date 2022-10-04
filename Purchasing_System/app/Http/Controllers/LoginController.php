1b634b1c3d75e0becf3085be6d7ad7b5e25c658b
Muhammad Afifudin <muhafifudin2306@gmail.com>
2022-10-05 06:32:04 +0700

    Fix bug auth

--- Purchasing_System/app/Http/Controllers/LoginController.php
+++ Purchasing_System/app/Http/Controllers/LoginController.php
@@ -2,7 +2,10 @@
 
 namespace App\Http\Controllers;
 
+
 use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Auth;
 
 class LoginController extends Controller
 {
