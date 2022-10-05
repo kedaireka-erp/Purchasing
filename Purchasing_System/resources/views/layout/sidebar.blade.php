bf864a586434e0edc2d03c35477942926c58e33c
Muhammad Afifudin <72702650+Muhafifudin2306@users.noreply.github.com>
2022-10-05 08:36:21 +0700

    Update sidebar.blade.php

--- Purchasing_System/resources/views/layout/sidebar.blade.php
+++ Purchasing_System/resources/views/layout/sidebar.blade.php
@@ -91,7 +91,12 @@
                                             <polyline points="16 17 21 12 16 7"></polyline>
                                             <line x1="21" y1="12" x2="9" y2="12"></line>
                                         </svg>
-                                        <span class="ms-2">Logout </span>
+                                        <span class="ms-2">
+                                         <form id="logout-form" action="{{ url('/logout') }}" method="POST">
+                                        @csrf
+                                            <button type="submit" class="dropdown-item"><i class="dw dw-logout"></i>Log Out</button>
+                                           </form>
+                                        </span>
                                     </a>
                                 </div>
                             </li>
