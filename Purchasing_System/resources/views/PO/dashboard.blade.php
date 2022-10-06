229dd6b66664cb660855f29e0fdfffadd1c98c13
Muhammad Afifudin <72702650+Muhafifudin2306@users.noreply.github.com>
2022-10-06 07:10:19 +0700

    Update dashboard.blade.php

--- Purchasing_System/resources/views/PO/dashboard.blade.php
+++ Purchasing_System/resources/views/PO/dashboard.blade.php
@@ -47,7 +47,6 @@
                             <td width="15%">Alamat Kirim</td>
                             <td width="10%">Pembayaran</td>
                             <td width="15%">Waktu Pengiriman</td>
-                            <td width="15%">Status</td>
                             <td width="15%">Tanggal Pembuatan</td>
                             <td width="5%"></td>
                         </tr>
