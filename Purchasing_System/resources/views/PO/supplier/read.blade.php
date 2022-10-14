e4310868149deb514399e6ff8c1b0d4dd8e6f500
Muhammad Afifudin <muhafifudin2306@gmail.com>
2022-10-14 10:46:21 +0700

    Try Fixing Dropdown

--- Purchasing_System/resources/views/PO/supplier/read.blade.php
+++ Purchasing_System/resources/views/PO/supplier/read.blade.php
@@ -1,19 +1,20 @@
 
-<!-- Select2 CSS --> 
+{{-- <!-- Select2 CSS --> 
 <link href="{{ asset('layout/select2.css') }}" rel="stylesheet" /> 
 <!-- jQuery --> <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script> 
 <!-- Select2 JS --> 
-<script src="https://cdn.jsdelivr.net/npm/select2@4.1.0-beta.1/dist/js/select2.min.js"></script>
-
-<select class="form-select input-rounded form-control wide mb-3" name="id_supplier" id="id_supplier">
+<script src="https://cdn.jsdelivr.net/npm/select2@4.1.0-beta.1/dist/js/select2.min.js"></script> --}}
+<x-style></x-style>
+<select class="nice-select default-select form-control wide mb-3" name="id_supplier" id="id_supplier">
     <option selected disabled>-- Pilih Tipe --</option>
     @foreach ($supplier as $item)
         <option value="{{ $item->id }}">{{ $item->vendor }}</option>
     @endforeach
 </select>
-<script>
+<x-script></x-script>
+{{-- <script>
     $(document).ready(function(){
         $("#id_supplier").select2();
     });
-</script>
+</script> --}}
 
