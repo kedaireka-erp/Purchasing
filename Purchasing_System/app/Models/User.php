56e7efbda879dc362c60ac6f919ba3c9c845250c
Muhammad Afifudin <muhafifudin2306@gmail.com>
2022-10-24 08:54:34 +0700

    Update User.php

--- Purchasing_System/app/Models/User.php
+++ Purchasing_System/app/Models/User.php
@@ -4,7 +4,7 @@ namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
-use Illuminate\Permission\Traits\HasRoles;
+use Spatie\Permission\Traits\HasRoles;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
